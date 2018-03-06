2851eaad58dc2c8b86a77a1a065d4586da42f889
eruzanov <evgeniy.ruzanov@piano.io>
2018-03-06 17:52:20 +0300

    generate random data

--- src/App.js
+++ src/App.js
@@ -1,56 +1,87 @@
 import React, {Component} from 'react';
-import {VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryStack} from 'victory';
+import {VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryStack, VictoryContainer} from 'victory';
 
-const data2012 = [
-  {quarter: 1, earnings: 13000},
-  {quarter: 2, earnings: 16500},
-  {quarter: 3, earnings: 14250},
-  {quarter: 4, earnings: 19000}
-];
+const WIDTH = 800;
+const HEIGHT = 300;
 
-const data2013 = [
-  {quarter: 1, earnings: 15000},
-  {quarter: 2, earnings: 12500},
-  {quarter: 3, earnings: 19500},
-  {quarter: 4, earnings: 13000}
-];
+const DATA = [];
+let i = 30;
+while (i--) DATA.push({x: 10 * i, y: Math.round(Math.random() * 1000)});
 
-const data2014 = [
-  {quarter: 1, earnings: 11500},
-  {quarter: 2, earnings: 13250},
-  {quarter: 3, earnings: 20000},
-  {quarter: 4, earnings: 15500}
-];
+let DATA_STACK = [];
+i = 30;
+while (i--) DATA_STACK.push({x: 10 * i, y: -(Math.round(Math.random() * 500))});
+DATA_STACK = DATA_STACK.reverse();
 
-const data2015 = [
-  {quarter: 1, earnings: 18000},
-  {quarter: 2, earnings: 13250},
-  {quarter: 3, earnings: 15000},
-  {quarter: 4, earnings: 12000}
-];
+const tickValues = (start = -900, stop = 900, step = 150) => {
+  const result = [];
+  let index = start;
+  while (index < stop) {
+    result.push(index);
+    index += step;
+  }
+  return result;
+};
+
+const NoDraw = () => null;
 
 class App extends Component {
   render() {
+    VictoryTheme.material.axis.style.grid.strokeDasharray = null;
+    VictoryTheme.material.axis.style.axis.stroke = 'transparent';
+    VictoryTheme.material.bar.style.data.fill = 'url(#grad-blue)';
     return (
-      <VictoryChart
-        domainPadding={20}
-        theme={VictoryTheme.material}
-      >
-        <VictoryAxis
-          tickValues={[1, 2, 3, 4]}
-          tickFormat={value => `Quarter ${value}`}
-        />
-        <VictoryAxis
-          dependentAxis
-          tickFormat={x => `$${x / 1000}k`}
-        />
-        <VictoryStack colorScale={'warm'}>
-          <VictoryBar data={data2012} x="quarter" y="earnings"/>
-          <VictoryBar data={data2013} x="quarter" y="earnings"/>
-          <VictoryBar data={data2014} x="quarter" y="earnings"/>
-          <VictoryBar data={data2015} x="quarter" y="earnings"/>
-        </VictoryStack>
-      </VictoryChart>
+      <div>
+        <svg>
+          <defs>
+            <linearGradient id="grad-blue">
+              <stop offset="0%" stopColor="rgb(116, 147, 161)" stopOpacity="0.4"/>
+              <stop offset="100%" stopColor="rgb(116, 147, 161)" stopOpacity="0.8"/>
+            </linearGradient>
+            <linearGradient id="grad-red">
+              <stop offset="0%" stopColor="rgb(183, 76, 81)" stopOpacity="0.3"/>
+              <stop offset="100%" stopColor="rgb(183, 76, 81)" stopOpacity="0.6"/>
+            </linearGradient>
+            <linearGradient id="grad-orange">
+              <stop offset="0%" stopColor="rgb(209, 108, 36)" stopOpacity="0.3"/>
+              <stop offset="100%" stopColor="rgb(209, 108, 36)" stopOpacity="0.6"/>
+            </linearGradient>
+            <linearGradient id="grad-yellow">
+              <stop offset="0%" stopColor="rgb(204, 162, 55)" stopOpacity="0.3"/>
+              <stop offset="100%" stopColor="rgb(204, 162, 55)" stopOpacity="0.6"/>
+            </linearGradient>
+          </defs>
+        </svg>
+        <VictoryChart
+          width={WIDTH}
+          height={HEIGHT}
+          theme={VictoryTheme.material}
+          containerComponent={<VictoryContainer width={WIDTH} height={HEIGHT}/>}
+        >
+          <VictoryAxis
+            offsetY={50}
+            gridComponent={<NoDraw/>}
+            tickCount={DATA.length}
+            tickFormat={v => `2/${v / 10}`}
+          />
+          <VictoryAxis
+            dependentAxis
+            tickValues={tickValues()}
+          />
+          <VictoryAxis
+            dependentAxis
+            orientation="right"
+            tickValues={tickValues()}
+            tickFormat={v => `${v / 100}%`}
+          />
+          <VictoryBar data={DATA} fill="url(#data-grad)"/>
+          <VictoryStack colorScale={['url(#grad-red)', 'url(#grad-orange)', 'url(#grad-yellow)']}>
+            <VictoryBar data={DATA_STACK}/>
+            <VictoryBar data={DATA_STACK}/>
+            <VictoryBar data={DATA_STACK}/>
+          </VictoryStack>
+        </VictoryChart>
+      </div>
     );
   }
 }
